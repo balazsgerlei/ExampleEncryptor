f7727137bcc9a379d3abb4d16d0b185fcf1f2f96
Balázs Gerlei <gerlotdev@gmail.com>
2025-08-21 20:43:41 +0200

    Update AndroidX Biometric library to the latest, 1.4.0-alpha04 and use the new AuthenticationRequest API
    - Instead of using the BiometricPrompt and it's callback, register an AuthenticationResultLauncher and use it to launch an AuthenticationRequest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.biometric.ktx)
+    implementation(libs.androidx.biometric)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
